95e6bc46014e0d3bb80425176bf1dff8d21dc676
Gaabe-santos <gabriel.sts0@outlook.com>
2022-10-18 21:44:53 -0300

    Segundo commit

--- src/main/java/System/demo/Controller.java
+++ src/main/java/System/demo/Controller.java
@@ -1,10 +1,7 @@
 package System.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/cliente/v1/")
@@ -13,11 +10,17 @@ public class Controller {
     @Autowired
     Repository repository;
 
-
     @PostMapping
     public  Cliente create(@RequestBody Cliente cliente){
         Cliente clienteSaved = repository.save(cliente);
         return  clienteSaved;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Cliente getClienteById(@PathVariable Long id){
+        Cliente  clienteReturned = repository.getById(id);
+        return clienteReturned;
     }
 
 }
